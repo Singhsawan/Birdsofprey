92dcad606a42662f9e3773400d621d75e20717e4
Akhand Bharat <125857863+Singhsawan@users.noreply.github.com>
2024-09-01 09:26:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,11 +48,11 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/Howto_openlink_0")
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'publicearn.com/')
 SHORTLINK_API = environ.get('SHORTLINK_API', '1d578e33166684d83f91c374bb9d5dd330eb3940')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002107694899').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
